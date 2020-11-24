aaeae87e72bcaf5fe74e438d7c17e4c2d9f39601
Rafael Chianca <rafachianca@gmail.com>
2020-11-24 18:05:12 -0300

    Creates news list page

---
+++ web/src/components/News.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from 'react';
+import firebase from '../firebase';
+import NavBar from './NavBar';
+
+const News = () => {
+  const [noticias, setNoticias] = useState([]);
+
+  useEffect(() => {
+    const db = firebase.firestore();
+
+    const collection = db.collection('noticias');
+
+    const noticiasFirestore = [];
+    collection.onSnapshot((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        noticiasFirestore.push({
+          id: doc.id,
+          ...doc.data(),
+        });
+      });
+      setNoticias(noticiasFirestore);
+    });
+  }, []);
+
+  return (
+    <>
+      <NavBar />
+      <div
+        style={{
+          display: 'flex',
+          width: '100%',
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <div
+          style={{
+            width: '30%',
+            height: '100%',
+          }}
+        />
+        <div
+          style={{
+            display: 'flex',
+            width: '100%',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          {noticias && noticias.length ? (
+            noticias.map((noticia) => {
+              return (
+                <div
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    width: '100%',
+                    marginBottom: 20,
+                    borderBottomWidth: 2,
+                    borderBottomColor: 'gray',
+                    borderBottomStyle: 'solid',
+                  }}
+                >
+                  <h2 style={{ margin: 0 }}>{noticia.titulo}</h2>
+                  <h4 style={{ margin: 0 }}>{noticia.subtitulo}</h4>
+                  <p style={{ margin: 0, marginBottom: 20 }}>
+                    Escrito por: {noticia.autor}
+                  </p>
+                  <h3 style={{ margin: 0 }}>{noticia.corpo}</h3>
+                </div>
+              );
+            })
+          ) : (
+            <></>
+          )}
+        </div>
+        <div style={{ width: '30%', height: '100%' }} />
+      </div>
+    </>
+  );
+};
+
+export default News;
--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -4,12 +4,14 @@ import Route from './Route';
 import NewsForm from '../components/NewsForm/NewsForm';
 import SignUp from '../components/SignUp';
 import SignIn from '../components/SignIn';
+import News from '../components/News';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/signup" component={SignUp} />
+      <Route path="/news" component={News} isPrivate />
       <Route path="/registerNews" component={NewsForm} isPrivate />
       {/* <Route path="/dashboard" component={Dashboard} isPrivate /> */}
     </Switch>
