539c60f347bb32685187b9f780ace0841823bb54
Rafael Chianca <rafachianca@gmail.com>
2020-11-24 03:30:54 -0300

    Changes style for sign up page

--- web/src/components/SignUp.jsx
+++ web/src/components/SignUp.jsx
@@ -22,30 +22,57 @@ const SignUp = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="Senha"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <input
-        type="text"
-        name="name"
-        placeholder="Nome"
-        value={userName}
-        onChange={(e) => setUserName(e.target.value)}
-      />
-      <button type="submit">Cadastrar</button>
-    </form>
+    <div
+      style={{
+        display: 'flex',
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <form
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          backgroundColor: 'lightgray',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderWidth: 1,
+          borderRadius: 4,
+          borderColor: 'black',
+          alignSelf: 'center',
+          padding: 20,
+        }}
+        onSubmit={handleSubmit}
+      >
+        <h2>Login</h2>
+        <input
+          style={{ width: 200, marginBottom: 20 }}
+          type="text"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          style={{ width: 200, marginBottom: 20 }}
+          type="password"
+          name="password"
+          placeholder="Senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          style={{ width: 200, marginBottom: 20 }}
+          type="text"
+          name="name"
+          placeholder="Nome"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <button type="submit">Cadastrar</button>
+      </form>
+    </div>
   );
 };
 
