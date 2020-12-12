475827e24649c953219f764580bff2b7d51909ec
Rafael Chianca <rafachianca@gmail.com>
2020-12-12 18:29:59 -0300

    Adds test for singnup with firebase
    Co-authored-by: João Marcelo <jmarcelonunes738@gmail.com>

--- web/src/tests/index.test.js
+++ web/src/tests/index.test.js
@@ -93,4 +93,18 @@ describe('Firebase test suite', () => {
     expect(firestoreMock.mockAuth).toHaveBeenCalled()
     done()
   })
+
+  it('signup to firebase is called', (done) => {
+    const firestoreMock = new FirestoreMock();
+    firebase.auth = firestoreMock.mockAuth;
+    firestoreMock.reset();
+
+    firestoreMock.mockAuthReturn = { id: 'auth-test-id' }
+    firebase
+    .auth()
+    .createUserWithEmailAndPassword
+
+    expect(firestoreMock.mockAuth).toHaveBeenCalled()
+    done()
+  })
 })
