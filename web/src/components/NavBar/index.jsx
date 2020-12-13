ab74d50f9b6f84f48b1a5a34a99437dc6c452bc4
Rafael Chianca <rafachianca@gmail.com>
2020-12-12 23:04:13 -0300

    Layout changes to navbar component

--- web/src/components/NavBar/index.jsx
+++ web/src/components/NavBar/index.jsx
@@ -1,34 +1,37 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import { AddBox, Description, MeetingRoom } from '@material-ui/icons';
+import Typography from '@material-ui/core/Typography';
 import { useAuth } from '../../hooks/auth';
-import { Container, LogoutButton, NavItem } from './style';
+import { NavItem } from './style';
 
 const NavBar = () => {
   const { signOut } = useAuth();
-
   return (
-    <Container data-testid="navbar">
-      <div
-        style={{
-          display: 'flex',
-          flex: 1,
-          height: '100%',
-          alignItems: 'center',
-        }}
-      >
-        <NavItem to="/news">
-          Notícias
-        </NavItem>
-        <NavItem to="/registerNews">
-          Cadastrar Notícia
-        </NavItem>
-      </div>
-      <LogoutButton
-        type="button"
-        onClick={signOut}
-      >
-        Sair
-      </LogoutButton>
-    </Container>
+    <AppBar position="static" style={{ backgroundColor: '#005484' }}>
+      <Toolbar style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div>
+          <Button color="inherit">
+            <NavItem to="/news">
+              <Description />
+              <Typography>Notícias</Typography>
+            </NavItem>
+          </Button>
+          <Button color="inherit">
+            <NavItem to="/registerNews">
+              <AddBox />
+              <Typography>Cadastrar Notícias</Typography>
+            </NavItem>
+          </Button>
+        </div>
+        <Button color="inherit" onClick={signOut}>
+          <MeetingRoom />
+          <Typography>Sair</Typography>
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
 };
 
--- web/src/components/NavBar/style.jsx
+++ web/src/components/NavBar/style.jsx
@@ -1,39 +1,10 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-export const Container = styled.div`
-    display: flex;
-    height: 50px;
-    width: 100%;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-    background-color: #367fa9;
-    margin-bottom: 30px;
-`;
-
 export const NavItem = styled(Link)`
-    height: 100%;
-    border-style: solid;
-    border-width: 0px;
-    border-right-width: 2px;
-    border-right-color: #005484;
     color: white;
-    padding: 20px;
     text-decoration: none;
     display: flex;
     align-items: center;
-`;
-
-export const LogoutButton = styled.button`
-    width: 80px;
-    justify-self: flex-end;
-    background-color: transparent;
-    border-style: solid;
-    height: 100%;
-    color: white;
-    border-top-width: 0px;
-    border-bottom-width: 0px;
-    border-right-width: 0px;
-    border-color: #005484;
+    justify-content: center;
 `;
