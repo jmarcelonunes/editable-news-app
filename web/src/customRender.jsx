11a276c4aa77eedd26a8d1dc7e291c24b4873785
Rafael Chianca <rafachianca@gmail.com>
2020-12-12 23:03:21 -0300

    Changes file extension

---
+++ web/src/customRender.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import AppProvider from './hooks/index';
+
+const Wrapper = ({ children }) => (
+  <AppProvider>
+    <MemoryRouter>{children}</MemoryRouter>
+  </AppProvider>
+);
+
+const customRender = (ui, options) => render(ui, { wrapper: Wrapper, ...options });
+
+// re-export everything
+export * from '@testing-library/react';
+
+// override render method
+export { customRender as render };
