cf0631e3e139be633bc02c9ab19a5d8698a59290
Rafael Chianca <rafachianca@gmail.com>
2020-12-13 01:10:01 -0300

    Implements styled components for news page

--- web/src/components/News/style.jsx
+++ web/src/components/News/style.jsx
@@ -1,19 +1,49 @@
+import { Button } from '@material-ui/core';
 import styled from 'styled-components';
 
-export const DeleteButton = styled.button`
-    float: right;
-    background-color: #dd4b39 !important;
-    color: white;
-    display: inline;
-    padding: .2em .6em .3em;
-    font-size: 75%;
-    font-weight: 700;
-    line-height: 1;
-    text-align: center;
-    white-space: nowrap;
-    border: 0;
-    background: none;
-    box-shadow: none;
-    border-radius: 0px;
-    vertical-align: baseline;
+export const MainContentContainer = styled.div`
+    display: flex;
+    width: 100%;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+`;
+
+export const CenterContainer = styled.div`
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const NewsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    margin-bottom: 20px;
+    border-bottom-width: 2px;
+    border-bottom-style: solid;
+    border-bottom-color: #005484;
+    padding-bottom: 20px;
+`;
+
+export const ImageContainer = styled.div`
+    margin-bottom: 20px;
+    height: 400px;
+    background-repeat: no-repeat;
+    background-size: contain;
+`;
+
+export const DeleteButton = styled(Button)`
+  background-color: #ff0000 !important;
+  color: white !important;
+  float: right !important;
+`;
+
+export const EditButton = styled(Button)`
+  background-color: lightgray !important;
+  float: right !important;
+  margin-right: 20px !important;
 `;
